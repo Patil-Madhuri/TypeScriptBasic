0f65162032786ff57962274a6843432caac613ac
madhuripatil506 <madhuripatil506@gmail.com>
2018-06-18 18:11:30 +0530

    uniontype added

---
+++ UnionType.js
@@ -0,0 +1,36 @@
+// Union type means ability to combine one or two types.
+// Two or more data types are combined using the pipe symbol (|) to denote a Union Type
+//Union type variable
+var val;
+val = 12;
+console.log("numeric value of val " + val);
+val = "This is a string";
+console.log("string value of val " + val);
+//Union type function
+// disp() can accept argument either of the type string or a string array.
+function disp(name) {
+    if (typeof name == "string") {
+        console.log(name);
+    }
+    else {
+        for (var i = 0; i < name.length; i++) {
+            console.log(name[i]);
+        }
+    }
+}
+function display(number) {
+    if (typeof number == "number") {
+        console.log(number);
+    }
+    else {
+        for (var i = 0; i < number.length; i++) {
+            console.log(number[i]);
+        }
+    }
+}
+disp("madhuri");
+console.log("From disp method.................");
+disp(["madhuri", "ankita", "rohini", "shweta", "poonam"]);
+display(20);
+console.log("From display method..............");
+display(["madhuri", "ankita", "rohini", "shweta", "poonam"]);
